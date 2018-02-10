7bd197e22bac52d9175c247c864fa87236a08e25
Bart Smet <bartsmet30@gmail.com>
2018-02-10 15:30:21 +0100

    Feat: Cleaning up code, removing dead code

--- app.js
+++ app.js
@@ -6,35 +6,32 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const expressLayout = require('express-ejs-layouts');
 
-const index = require('./routes/index.routes');
 const auth = require('./routes/auth.routes');
-// const users = require('./routes/users.routes');
-// const profile = require('./routes/profile.routes');
+
 
 const app = express();
 
 // Require DATABASE
 require('./config/config.db.js');
 
+// PASSPORT MIDDLEWARE HERE
+
 // view engine setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayout);
-app.set('layout', 'layout');
-
+app.set('layout', 'layouts/main');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/auth', auth);
-// app.use('/users', users);
-// app.use('/profile', profile);
+app.use('/', auth);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,14 +41,14 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error.views');
+  res.render('error');
 });
 
 module.exports = app;
--- routes/profile.routes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profile.controller');
-
-module.exports = router;
