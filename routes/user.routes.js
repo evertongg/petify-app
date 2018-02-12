640e3eed17e5a0153bb0141290df2349fa1f2eb2
Bart Smet <bartsmet30@gmail.com>
2018-02-12 21:46:34 +0100

    Feat: Added edit view and basic lay-out

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.get('/', userController.show);
+router.get('/edit', userController.edit);
 
 module.exports = router;
