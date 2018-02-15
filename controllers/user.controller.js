38944625f45cffd1eb7f614213b76516ae036052
Bart Smet <bartsmet30@gmail.com>
2018-02-15 20:44:57 +0100

    Feat: Upload pictures function, creating routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
 const User = require('../models/user.model');
 const Post = require('../models/posts.model');
+const Picture = require('../models/picture.model');
 const moment = require('moment');
-
 
 module.exports.show = (req, res) => {
   const {_id} = res.locals.user;
@@ -10,23 +10,46 @@ module.exports.show = (req, res) => {
   User.findById(_id)
   .then((user) => {
     // Find posts of user
-      Post.find({owner_id: user.id})
+      Post.find({owner_id: user._id})
       .then((posts) => {
-        res.render('user/user', {
-          posts: posts,
-        });
+        Picture.find({})
+        .then((pictures) => {
+          console.log(pictures)
+          res.render('user/user', {
+            user,
+            pictures,
+            posts
+          });
+        })
       })
-      .catch(err => console.log(err));
-
-    res.render('user/user', {
-      user,
-      posts
-    });
+      //.catch(err => console.log(err));
+    //
+    // res.render('user/user', {
+    //   user,
+    //   posts
+    // });
   })
   .catch(err => {
     console.log(err);
   });
 };
+
+// module.exports.showPictures = (req, res, next) => {
+// const {_id} = res.locals.user;
+//
+//   Picture.find({_id: user.id})
+//   .then((pictures) => {
+//     console.log(pictures);
+//     res.render('user/user', {
+//       user: user
+//       pictures: pictures
+//     });
+//   })
+//     .catch(err => console.log(err));
+//     res.render('user/user', {
+//       pictures
+//     })
+// };
 
 module.exports.edit = (req, res, next) => {
   res.render('user/edit');
@@ -34,4 +57,20 @@ module.exports.edit = (req, res, next) => {
 
 module.exports.addPost = (req, res, next) => {
   res.send('sending post');
+};
+
+
+
+module.exports.savePic = (req, res) => {
+  console.log(req.file);
+
+  const pic = new Picture({
+    name: req.body.name,
+    pic_path: `/uploads/${req.file.filename}`,
+    pic_name: req.file.originalname
+  });
+
+  pic.save((err) => {
+      res.redirect('/');
+  });
 };
