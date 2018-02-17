4795f49f7d8eb66428ace364a0ca2759cc05b5f8
LAURA MARCOS AGUILAR <Laura@MacBook-Pro-de-LAURA.local>
2018-02-17 17:29:52 +0100

    feat: add likes to posts

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -4,6 +4,7 @@ const postController = require('../controllers/post.controller');
 const secure = require('../middleware/secure.middleware');
 
 router.post('/:id', postController.addPost);
+router.get('/:id', postController.updatePost);
 
 
 module.exports = router;
