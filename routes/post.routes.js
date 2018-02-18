b2060dae3468ef139e77599f945f9ab8a03adabe
LAURA MARCOS AGUILAR <Laura@MacBook-Pro-de-LAURA.local>
2018-02-18 12:05:06 +0100

    añadido dropbox

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/post.controller');
 const secure = require('../middleware/secure.middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const exist = require('../middleware/exist.middleware');
 
 router.post('/:id', postController.addPost);
 router.get('/:id', postController.updatePost);
+router.post('/', exist.existsUploadsFolder, upload.single('photo'), postController.createPost);
+
 
 module.exports = router;
